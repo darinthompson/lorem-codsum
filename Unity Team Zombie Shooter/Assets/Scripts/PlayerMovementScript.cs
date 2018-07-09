13cbe3cfc41fd7adb92701e79d108e7188868f6e
Lucas <ljgecko1230@gmail.com>
2018-07-09 11:30:41 -0700

    Fixed camera and added player movement
    Camera follows player and is top-down style. Player moves at relatively slow speed.

---
+++ Unity Team Zombie Shooter/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+
+        transform.Translate(x, 0, 0);
+        transform.Translate(0, 0, z);
+	}
+}
